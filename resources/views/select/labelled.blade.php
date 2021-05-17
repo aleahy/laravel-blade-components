1fd5609b221a78720ec593425fe4a49b50d69de1
Alexander Leahy <alex@languagetub.com>
2021-05-17 14:46:25 +1000

    Updated border colour of labelled select

--- resources/views/select/labelled.blade.php
+++ resources/views/select/labelled.blade.php
@@ -1,7 +1,6 @@
 <div {{ $attributes->except('wire:model')->merge(['class' => 'flex items-center']) }}>
-{{--    {{ dd(get_class_methods($attributes)) }}--}}
-    <div class="py-3 px-5 flex flex-col items-center border-gray-400 border-t border-l border-b rounded-l-md bg-gray-50 text-sm whitespace-nowrap">{{ $label }}</div>
-    <select class="py-3 px-5 w-full border-gray-400 text-gray-800 rounded-r-md text-sm"
+    <div class="py-3 px-5 flex flex-col items-center border-gray-300 border-t border-l border-b rounded-l-md bg-gray-50 text-sm whitespace-nowrap">{{ $label }}</div>
+    <select class="py-3 px-5 w-full border-gray-300 text-gray-800 rounded-r-md text-sm"
         {{ $attributes->wire('model') }} >
         {{ $slot }}
     </select>
